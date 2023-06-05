0c52821d1555b8d5d685a206ccff6a1df287ccd9
ldu2175 <ldu2175@gmail.com>
2023-06-06 01:51:28 +0900

    add: motion layout on MainActivity and MatchingFragment

--- app/src/main/java/com/donxux/codate/presentation/view/MainActivity.kt
+++ app/src/main/java/com/donxux/codate/presentation/view/MainActivity.kt
@@ -3,8 +3,10 @@ package com.donxux.codate.presentation.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -13,12 +15,17 @@ import androidx.navigation.ui.setupWithNavController
 import com.donxux.codate.R
 import com.donxux.codate.databinding.ActivityMainBinding
 import com.donxux.codate.presentation.viewmodel.MainViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var mainViewModel: MainViewModel
     private lateinit var navController: NavController
+
+    val motionProgress = Channel<Float>()
+    var lockedNavigate = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -28,6 +35,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setMainViewModel()
         setBottomNavigation()
+        setRootLayoutTransitionListener()
+    }
+
+    private fun setRootLayoutTransitionListener() {
+        binding.mainRootLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+                lockedNavigate = true
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+                lifecycleScope.launch {
+                    motionProgress.send(progress)
+                }
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                lockedNavigate = false
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) = Unit
+        })
+    }
+
+    private fun lockMotionOfRootLayout() {
+        binding.mainRootLayout.setTransition(R.id.start, R.id.start)
+    }
+
+    private fun unlockMotionOfRootLayout() {
+        binding.mainRootLayout.setTransition(R.id.start, R.id.end)
     }
 
     private fun setMainViewModel() {
@@ -43,12 +93,25 @@ class MainActivity : AppCompatActivity() {
 
         binding.mainBottomNavigation.setOnItemSelectedListener {
             when (it.itemId) {
+                R.id.matching -> {
+                    unlockMotionOfRootLayout()
+                    it.onNavDestinationSelected(navController)
+                }
+
                 R.id.like, R.id.chat, R.id.info -> {
+                    lockMotionOfRootLayout()
                     goToLoginActivity()
                     false
                 }
 
-                else -> it.onNavDestinationSelected(navController)
+                else -> {
+                    if (lockedNavigate) {
+                        false
+                    } else {
+                        lockMotionOfRootLayout()
+                        it.onNavDestinationSelected(navController)
+                    }
+                }
             }
         }
     }
@@ -57,5 +120,10 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         overridePendingTransition(R.anim.slide_up, R.anim.stay_in_place)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        motionProgress.close()
     }
 }
--- app/src/main/java/com/donxux/codate/presentation/view/matching/MatchingFragment.kt
+++ app/src/main/java/com/donxux/codate/presentation/view/matching/MatchingFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.donxux.codate.databinding.FragmentMatchingBinding
+import com.donxux.codate.presentation.view.MainActivity
 import com.donxux.codate.presentation.viewmodel.MatchingViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 
 class MatchingFragment : Fragment() {
@@ -22,10 +25,12 @@ class MatchingFragment : Fragment() {
 
     private lateinit var partnerCardsAdapter: PartnerCardsAdapter
 
+    private var motionProgress: Channel<Float>? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentMatchingBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this)[MatchingViewModel::class.java]
@@ -35,15 +40,17 @@ class MatchingFragment : Fragment() {
             PartnerCardsAdapter(requireActivity(), partners.toMutableList())
         binding.searchPartnerCardsPager.adapter = partnerCardsAdapter
 
+        setReceiveProgressOfMainActivityMotion()
+
         binding.searchPartnerCardsPager.registerOnPageChangeCallback(object :
-                OnPageChangeCallback() {
-                override fun onPageSelected(position: Int) {
-                    super.onPageSelected(position)
-                    if (position == partners.size) {
-                        viewModel.updatePartners()
-                    }
+            OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == partners.size) {
+                    viewModel.updatePartners()
                 }
-            })
+            }
+        })
 
         lifecycleScope.launch {
             viewModel.partners.collect {
@@ -51,8 +58,44 @@ class MatchingFragment : Fragment() {
                 partnerCardsAdapter.updateItems(it.toMutableList())
             }
         }
+        setRootLayoutTransitionListener()
 
         return binding.root
+    }
+
+    private fun setReceiveProgressOfMainActivityMotion() {
+        motionProgress = (requireActivity() as MainActivity).motionProgress
+        lifecycleScope.launch {
+            for (progress: Float in motionProgress!!) {
+                binding.matchingRootLayout.progress = progress
+            }
+        }
+    }
+
+    private fun setRootLayoutTransitionListener() {
+        binding.matchingRootLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+            ) = Unit
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float,
+            ) = Unit
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) = Unit
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float,
+            ) = Unit
+        })
     }
 
     override fun onDestroyView() {
